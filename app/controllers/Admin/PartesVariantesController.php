f9371d56f6fe293d293b5cbfbc6dbef2fe910739
Gigliotti Martin <martin@unik.ar>
2026-03-07 15:40:05 -0300

    chore: bump version to v8.4.0 build 3911

--- app/controllers/Admin/PartesVariantesController.php
+++ app/controllers/Admin/PartesVariantesController.php
@@ -146,12 +146,12 @@ final class PartesVariantesController extends Controller
             ]);
         }
 
-        $this->variantes->create($data);
+        $newVariantId = $this->variantes->create($data);
 
         // Check if coming from Manager (via Context param)
         $context = $request->input('context');
         if ($context === 'manager') {
-            return Response::redirect(url("/productos/partes/manager/{$idParte}/editar"));
+            return Response::redirect(url("/productos/partes/manager/{$idParte}/variantes/{$newVariantId}"));
         }
 
         return Response::redirect(url('/productos/partes?tab=variantes&id_parte=' . $idParte));

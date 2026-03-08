0499d66e6e09ddb0700aa2399c612b5580aa8e77
Gigliotti Martin <martin@unik.ar>
2026-03-08 12:19:28 -0300

    chore: bump version to v9.7.1 build 3920

--- config/app.php
+++ config/app.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => env('APP_NAME', 'MRP'),
-    'version' => '9.7.0',
-    'build' => 3919,
+    'version' => '9.7.1',
+    'build' => 3920,
     'url' => env('APP_URL', 'http://localhost'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
@@ -12,6 +12,7 @@ return [
     'session_name' => env('SESSION_NAME', 'MRPSESSID'),
     'asset_prefix' => parse_url(env('APP_URL', 'http://localhost'), PHP_URL_PATH) ?? '',
 ];
+
 
 
 

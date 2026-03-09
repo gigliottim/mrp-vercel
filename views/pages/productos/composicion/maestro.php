081c2878b76168b3f93748efbc44234932323cc6
Gigliotti Martin <martin@unik.ar>
2026-03-08 23:07:11 -0300

    chore: bump version to v12.10.0 build 3948

--- views/pages/productos/composicion/maestro.php
+++ views/pages/productos/composicion/maestro.php
@@ -225,7 +225,9 @@ unset($_SESSION['bom_error'], $_SESSION['bom_success']);
     .details-panel-body {
         flex: 1 1 auto;
         min-height: 0;
-        overflow-y: auto;
+        height: 0;
+        overflow-y: scroll;
+        overflow-x: hidden;
     }
 
     .details-selected-content {

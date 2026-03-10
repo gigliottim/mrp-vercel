3808f7e46ff6ba6b9c156edca22a266e3cb8231b
Gigliotti Martin <martin@unik.ar>
2026-03-10 00:08:03 -0300

    chore: bump version to v17.9.1 build 4003

--- config/app.php
+++ config/app.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => env('APP_NAME', 'MRP'),
-    'version' => '17.9.0',
-    'build' => 4002,
+    'version' => '17.9.1',
+    'build' => 4003,
     'url' => env('APP_URL', 'http://localhost'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
@@ -12,6 +12,7 @@ return [
     'session_name' => env('SESSION_NAME', 'MRPSESSID'),
     'asset_prefix' => parse_url(env('APP_URL', 'http://localhost'), PHP_URL_PATH) ?? '',
 ];
+
 
 
 

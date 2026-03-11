529ed26413b079e1bd4423a5e0d025d5d5c2cbb2
Gigliotti Martin <martin@unik.ar>
2026-03-11 17:53:27 -0300

    chore: deploy v29.1.1 build 4038

--- config/app.php
+++ config/app.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => env('APP_NAME', 'MRP'),
-    'version' => '29.1.0',
-    'build' => 4037,
+    'version' => '29.1.1',
+    'build' => 4038,
     'url' => env('APP_URL', 'http://localhost'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),

c4e8b8037ea834209d01f390cf66bcb58e79b5a1
Gigliotti Martin <martin@unik.ar>
2026-03-10 22:41:22 -0300

    chore: deploy v27.0.5 build 4030

--- config/app.php
+++ config/app.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => env('APP_NAME', 'MRP'),
-    'version' => '27.0.4',
-    'build' => 4029,
+    'version' => '27.0.5',
+    'build' => 4030,
     'url' => env('APP_URL', 'http://localhost'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
@@ -12,6 +12,7 @@ return [
     'session_name' => env('SESSION_NAME', 'MRPSESSID'),
     'asset_prefix' => parse_url(env('APP_URL', 'http://localhost'), PHP_URL_PATH) ?? '',
 ];
+
 
 
 

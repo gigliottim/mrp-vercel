2b10f3347a52831e2e5c84ebb0b388ea5dd7a656
Gigliotti Martin <martin@unik.ar>
2026-03-11 21:51:49 -0300

    chore: deploy v30.7.2 build 4055

--- config/app.php
+++ config/app.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => env('APP_NAME', 'MRP'),
-    'version' => '30.7.1',
-    'build' => 4054,
+    'version' => '30.7.2',
+    'build' => 4055,
     'url' => env('APP_URL', 'http://localhost'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),

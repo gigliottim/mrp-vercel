510144ea5a5266a5c36b8d0d26a167fea404fa3d
Gigliotti Martin <martin@unik.ar>
2026-03-21 19:23:23 -0300

    chore: deploy v36.2.1 build 4097

--- config/app.php
+++ config/app.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => env('APP_NAME', 'MRP'),
-    'version' => '36.2.0',
-    'build' => 4096,
+    'version' => '36.2.1',
+    'build' => 4097,
     'url' => env('APP_URL', 'http://localhost'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),

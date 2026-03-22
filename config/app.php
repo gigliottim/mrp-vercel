c906f2e71827662966457583147e8da59c655a02
Gigliotti Martin <martin@unik.ar>
2026-03-22 17:22:29 -0300

    chore: deploy v36.10.1 build 4114

--- config/app.php
+++ config/app.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => env('APP_NAME', 'MRP'),
-    'version' => '36.10.0',
-    'build' => 4113,
+    'version' => '36.10.1',
+    'build' => 4114,
     'url' => env('APP_URL', 'http://localhost'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),

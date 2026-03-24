cf30acf6b326088fd97b57553bdbeb49949fe459
Gigliotti Martin <martin@unik.ar>
2026-03-24 01:17:53 -0300

    chore: deploy v37.4.5 build 4139

--- config/app.php
+++ config/app.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => env('APP_NAME', 'MRP'),
-    'version' => '37.4.4',
-    'build' => 4138,
+    'version' => '37.4.5',
+    'build' => 4139,
     'url' => env('APP_URL', 'http://localhost'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),

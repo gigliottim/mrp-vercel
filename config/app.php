c442bd17dd437e88c32cc3bd161d5b0cacbde621
Deploy Script <deploy@mrp.local>
2026-04-03 17:17:21 -0300

    chore: deploy v42.0.3 build 4162

--- config/app.php
+++ config/app.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => env('APP_NAME', 'MRP'),
-    'version' => '42.0.2',
-    'build' => 4161,
+    'version' => '42.0.3',
+    'build' => 4162,
     'url' => env('APP_URL', 'http://localhost'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),

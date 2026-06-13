10d7f1db36d9ec6a945a93051dfd61ec625f7574
Deploy Script <deploy@mrp.local>
2026-06-13 18:30:40 -0300

    chore: deploy v46.0.7 build 4203

--- views/components/agentAI/_agent_floating_button.php
+++ views/components/agentAI/_agent_floating_button.php
@@ -94,5 +94,5 @@ use App\Core\View\View;
 </div>
 
 <!-- CSS y JS externalizados del botón flotante -->
-<link rel="stylesheet" href="/agenteAI/frontend/css/agent_floating.css">
-<script src="/agenteAI/frontend/js/agent_floating.js" defer></script>
+<link rel="stylesheet" href="/agenteAI/frontend/css/agent_floating.css?v=46.0.6">
+<script src="/agenteAI/frontend/js/agent_floating.js?v=46.0.6" defer></script>

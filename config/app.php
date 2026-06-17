f9ea0449b7169c68fa4670222bf173b426abfc65
Deploy Script <deploy@mrp.local>
2026-06-17 16:07:20 -0300

    chore: deploy v52.7.1 build 4265

--- config/app.php
+++ config/app.php
@@ -2,8 +2,8 @@
 
 return [
     'name' => env('APP_NAME', 'MRP'),
-    'version' => '52.7.0',
-    'build' => 4264,
+    'version' => '52.7.1',
+    'build' => 4265,
     'url' => env('APP_URL', 'http://localhost'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
